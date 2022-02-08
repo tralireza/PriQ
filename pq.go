e581235bfa91c28d2c6110d740018fb1fecb7ee8
Alireza <26859725+tralireza@users.noreply.github.com>
2022-02-08 03:50:00 +0000

    857h Minimum Cost to Hire K Workers

--- pq.go
+++ pq.go
@@ -1,8 +1,10 @@
 package PriQ
 
 import (
+	"container/heap"
 	"log"
-	"math"
+	"slices"
+	"sort"
 )
 
 func init() {
@@ -11,9 +13,42 @@ func init() {
 	log.Print("> Priority Queue")
 }
 
+type PQ857 struct{ sort.IntSlice }
+
+func (pq PQ857) Less(i, j int) bool { return pq.IntSlice[i] > pq.IntSlice[j] }
+func (PQ857) Push(any)              {}
+func (PQ857) Pop() (_ any)          { return }
+
 // 857h Minimum Cost to Hire K Workers
 func mincostToHireWorkers(quality []int, wage []int, k int) float64 {
-	mCost := math.MaxFloat64
+	type WQ struct{ w, q int }
+	WQs := []WQ{}
 
+	for i, w := range wage {
+		WQs = append(WQs, WQ{w: w, q: quality[i]})
+	}
+	slices.SortFunc(WQs, func(a, b WQ) int { return a.w*b.q - b.w*a.q })
+	log.Print("W/Q -> ", WQs)
+
+	pq := PQ857{make([]int, k)}
+	tQ := 0
+	for i, v := range WQs[:k] {
+		pq.IntSlice[i] = v.q
+		tQ += v.q
+	}
+	heap.Init(&pq)
+	log.Print("PQ -> ", pq)
+
+	mCost := float64(tQ*WQs[k-1].w) / float64(WQs[k-1].q)
+	for _, v := range WQs[k:] {
+		if v.q < pq.IntSlice[0] {
+			tQ -= pq.IntSlice[0] - v.q
+
+			pq.IntSlice[0] = v.q
+			heap.Fix(&pq, 0)
+
+			mCost = min(mCost, float64(tQ*v.w)/float64(v.q))
+		}
+	}
 	return mCost
 }
--- pq_test.go
+++ pq_test.go
@@ -10,5 +10,5 @@ func init() {}
 // 857h Minimum Cost to Hire K Workders
 func Test857(t *testing.T) {
 	log.Print("105 ?= ", mincostToHireWorkers([]int{10, 20, 5}, []int{70, 50, 30}, 2))
-	log.Print("36.6 ?= ", mincostToHireWorkers([]int{3, 1, 1, 10, 1}, []int{4, 8, 2, 2, 7}, 3))
+	log.Print("30.6 ?= ", mincostToHireWorkers([]int{3, 1, 10, 10, 1}, []int{4, 8, 2, 2, 7}, 3))
 }
