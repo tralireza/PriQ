f2ac68eace2e3ec544abf38904df20dde4c6e6a4
Alireza <26859725+tralireza@users.noreply.github.com>
2022-02-10 18:14:00 +0000

    Priority Queue 786m K-th Smallest Prime Fraction -> O(k) Push(), Pop() with overall O(klogk)

--- pq_test.go
+++ pq_test.go
@@ -27,10 +27,11 @@ func Test786(t *testing.T) {
 
 	for _, f := range []func([]int, int) []int{kthSmallestPrimeFraction, On2} {
 		log.Print("[2 5] ?= ", f([]int{1, 2, 3, 5}, 3))
+		log.Print("[1 7] ?= ", f([]int{1, 7}, 1))
 	}
 }
 
-// 857h Minimum Cost to Hire K Workders
+// 857h Minimum Cost to Hire K Workers
 func Test857(t *testing.T) {
 	log.Print("105 ?= ", mincostToHireWorkers([]int{10, 20, 5}, []int{70, 50, 30}, 2))
 	log.Print("30.6 ?= ", mincostToHireWorkers([]int{3, 1, 10, 10, 1}, []int{4, 8, 2, 2, 7}, 3))
