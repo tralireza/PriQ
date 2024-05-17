095431a6cda193d170c6d444fe45c00cbf34f282
Alireza <26859725+tralireza@users.noreply.github.com>
2024-05-17 02:42:35 +0100

    Priority Queue

---
+++ pq.go
@@ -0,0 +1,19 @@
+package PriQ
+
+import (
+	"log"
+	"math"
+)
+
+func init() {
+	log.SetFlags(0)
+	log.SetPrefix("")
+	log.Print("> Priority Queue")
+}
+
+// 857h Minimum Cost to Hire K Workers
+func mincostToHireWorkers(quality []int, wage []int, k int) float64 {
+	mCost := math.MaxFloat64
+
+	return mCost
+}
